57193e63768180f680674c53b91965af75660a3f
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-07-12 10:43:40 -0500

    Starting the zoo

---
+++ Classes-Practice-Zoo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Classes_Practice_Zoo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ xUnit-Test-Zoo/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace xUnit_Test_Zoo;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
