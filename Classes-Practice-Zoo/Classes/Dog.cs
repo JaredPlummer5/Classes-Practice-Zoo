36185dd34804cacfdf7bdd309b7f9ae6b97bf9bf
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-07-19 14:52:45 -0500

    Lab 7 done

---
+++ Classes-Practice-Zoo/Classes/Dog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Classes_Practice_Zoo.Interfaces;
+
+namespace Classes_Practice_Zoo.Classes
+{
+	public class Dog : Mammal, IWalks
+	{
+		public Dog()
+		{
+        
+		}
+
+
+        public int Legs { get; set; }
+
+
+
+        public void Walk()
+        {
+            Console.WriteLine("Zoooom");
+        }
+
+
+        public override void I_am_a_Animal()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Sound()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
+
--- Classes-Practice-Zoo/Program.cs
+++ Classes-Practice-Zoo/Program.cs
@@ -26,6 +26,7 @@ class Program
         shark.Eat();
         shark.Sleep();
         shark.Sound();
+        shark.Swimming();
         Console.WriteLine("=======================");
 
         Flamingo flamingo = new Flamingo();
@@ -45,40 +46,8 @@ class Program
 
 
         Console.ReadLine();
-        //Animal Pig = new Animal();
-        //Pig.Name = "Porky";
-        //Pig.Legs = 4;
-        //Pig.Color = "Pink";
-        //Pig.IsEating = true;
-
-
-        //Console.WriteLine($"This animal is a pig. {Pig.Name} is {Pig.Color}. ....Is Eating? {Pig.IsEating} .....Is Sleepinging? {Pig.IsSleeping}");
-
-
-        //Animal Lizard = new Animal();
-        //Lizard.Name = "Liz";
-        //Lizard.Color = "Green";
-        //Lizard.IsSleeping = true;
-
-        //Console.WriteLine($"This animal is a pig. {Lizard.Name} is {Pig.Color}. ....Is Eating? {Lizard.IsEating} .....Is Sleepinging? {Lizard.IsSleeping}");
-
-        //Animal TrytaraHoneyBager = new Animal();
-        //TrytaraHoneyBager.Name = "Tracey";
-        //TrytaraHoneyBager.Color = "Gray";
-        //TrytaraHoneyBager.IsSleeping = true;
-
-        //Console.WriteLine($"This animal is a pig. {TrytaraHoneyBager.Name} is {TrytaraHoneyBager.Color}. ....Is Eating? {TrytaraHoneyBager.IsEating} .....Is Sleepinging? {TrytaraHoneyBager.IsSleeping}");
-
-
-        //Console.ReadLine();
     }
 
-    //public static string animalI(string testInput)
-    //{
-    //    Animal inout = new Animal();
-
-
-    //}
 
 }
 
